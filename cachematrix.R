5255033e02ede7d61a824e7904ffa6ab4cff4c0c
JJ <jianjun.lam@gmail.com>
2020-08-01 17:10:16 +0800

    Programming Assignment 2 for R Programming on Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -51,18 +51,3 @@ cacheSolve <- function(x, ...) {
     m
     ## Return a matrix that is the inverse of 'x'
 }
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
